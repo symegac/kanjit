80e627d48bc7681ed698d8d2ad38cb9db75cce71
symegac <97731141+symegac@users.noreply.github.com>
2025-06-19 21:07:09 +0200

    Add empty compiler

---
+++ compiler.py
@@ -0,0 +1,12 @@
+import llvmlite
+import llvmlite.ir
+
+"""
+Convert Lark AST to LLVM bytecode that can be (JIT)-compiled.
+"""
+
+def main() -> None:
+    pass
+
+if __name__ == "__main__":
+    main()
